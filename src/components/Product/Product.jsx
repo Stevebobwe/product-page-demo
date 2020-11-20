6fd24708acfd6a5e6869cd987a80bfbd6d3a0ce2
Steven Daily <Stevebobwe@gmail.com>
2020-11-19 22:26:09 -0800

    added displayPrice and first iteration of carousel images

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import './Product.scss';
 
 function Product(props) {
-  const { id, name, links, price, thumbnail, hero, images } = props;
+  const { id, name, links, price, priceRange, thumbnail, hero, images } = props;
+
+  // usd formatter
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  });
+
+  // get product price or priceRange
+  let displayPrice = price ? formatter.format(price.selling) : `${formatter.format(priceRange.selling.high)}-${formatter.format(priceRange.selling.low)}`;
+
+  // get carousel images
+  const imageCarousel = images
+    .map(productImage => {
+      return <img src={productImage.href} alt={name}></img>
+  });
 
   return (
     <li className="product">
         <img src={hero.href} alt={name}></img>
         <div>{name}</div>
+        <div>{displayPrice}</div>
+
+        {imageCarousel}
     </li>
   );
 }
