b35232e45479cbef17512336647457660579871f
Steven Daily <Stevebobwe@gmail.com>
2020-11-22 00:59:17 -0800

    moved header and footer into products component and added search

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './App.scss';
 import Products from '../Products/Products';
 import ProductModal from '../ProductModal/ProductModal';
-import { Button, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 
 function App() {  
   const [modal, setModal] = useState({
@@ -13,37 +12,7 @@ function App() {
 
   return (
     <React.Fragment>
-      {/* generic header */}
-      <Navbar expand="lg" className="sticky-top">
-        <div className="container-fluid">
-          <Navbar.Brand href="#home">Premium Furniture Company</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-dark">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </div>
-      </Navbar>
-
-      {/* main data */}
-      <div className="container-fluid">
-        <Products setModal={setModal} />
-      </div>
-
-      {/* generic footer */}
-      <div className="footer mt-auto py-3">
-        <div className="container-fluid">
-          <span className="fakeLink">Terms &amp; Conditions</span> | <span className="fakeLink">Privacy Policy</span> | <span className="fakeLink">Legal</span>
-        </div>
-      </div>
+      <Products setModal={setModal} />
 
       <ProductModal
         modal={modal}
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,31 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Products.scss';
 import ProductsData from '../../ProductsData';
 import Product from '../Product/Product';
+import { Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 
 function Products(props) {
+  const [byNameFilter, setByNameFilter] = useState('');
   const { setModal } = props;
-
   let products = ProductsData.groups;
-  //let filterFnsToApply = [];
+  let filterFnsToApply = [];
 
-  // name filter
-  // const [byNameFilter, setByNameFilter] = useState('');
+  // remove products whose names do not contain the input
+  if (byNameFilter) {
+    const byNameFilterFn = product => {
+      return product.name.toLowerCase().indexOf(byNameFilter.toLowerCase()) >= 0;
+    };
+    filterFnsToApply.push(byNameFilterFn);
+  }
 
-  // // remove products whose names do not contain the input
-  // if (byNameFilter) {
-  //   const byNameFilterFn = product => {
-  //     return product.name.toLowerCase().indexOf(byNameFilter.toLowerCase()) >= 0;
-  //   };
-  //   filterFnsToApply.push(byNameFilterFn);
-  // }
-
-  // // additional filters here
+  // additional filters here
   
-  // // apply filters to filteredProducts
-  // filterFnsToApply.forEach(filterFn => {
-  //   products = products.filter(filterFn);
-  // });
+  // apply filters to filteredProducts
+  filterFnsToApply.forEach(filterFn => {
+    products = products.filter(filterFn);
+  });
 
   // create <Product> for each remaining product in filteredProducts
   const filteredProducts = products
@@ -35,9 +33,41 @@ function Products(props) {
   });
 
   return (
-    <ul className="products row">
-      {filteredProducts}
-    </ul>
+    <React.Fragment>
+      {/* generic navbar */}
+      <Navbar expand="lg" className="sticky-top">
+        <div className="container-fluid">
+          <Navbar.Brand href="#home">Premium Furniture Company</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="navSearch mr-sm-2" onChange={ e => setByNameFilter(e.target.value) }/>
+              {/* <Button variant="outline-dark">Search</Button> */}
+            </Form>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+
+      {/* main */}
+      <div className="container-fluid">
+        <ul className="products row">
+          {filteredProducts}
+        </ul>
+      </div>
+
+      {/* generic footer */}
+      <div className="footer mt-auto py-3">
+        <div className="container-fluid">
+          <span className="fakeLink">Terms &amp; Conditions</span> | <span className="fakeLink">Privacy Policy</span> | <span className="fakeLink">Legal</span>
+        </div>
+      </div>      
+    </React.Fragment>
   );
 }
 
