c8fe09138022803c3eb62def2a85469bc73edf9b
Steven Daily <Stevebobwe@gmail.com>
2020-11-22 00:58:21 -0800

    updated

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders', () => {
   render(<App />);
-  const element = screen.getByText(/Premium Furniture Company/i);
-  expect(element).toBeInTheDocument();
 });
